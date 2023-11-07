dd5a2da454cbd28f722bd383d6e76e7f9b922178
oHTGo <nguyennhathuy.orit@gmail.com>
2023-11-08 00:56:14 +0700

    Update config

--- src/shared/configurations/twind.ts
+++ src/shared/configurations/twind.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from '@twind/core';
 import presetAutoprefix from '@twind/preset-autoprefix';
 import presetTailwind from '@twind/preset-tailwind';
+import type { Preset } from '@twind/core';
+
+const presetRemToPx = ({ baseValue = 16 } = {}): Preset => {
+  return {
+    finalize(rule) {
+      return {
+        ...rule,
+        d: rule.d?.replace(/"[^"]+"|'[^']+'|url\([^)]+\)|(-?\d*\.?\d+)rem/g, (match, p1) => {
+          if (!p1) return match;
+          return `${p1 * baseValue}${p1 == 0 ? '' : 'px'}`;
+        }),
+      };
+    },
+  };
+};
 
 export default defineConfig({
-  presets: [presetAutoprefix(), presetTailwind()],
+  presets: [presetAutoprefix(), presetTailwind(), presetRemToPx()],
 });
