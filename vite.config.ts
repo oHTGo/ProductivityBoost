3f294239acaa5d094c09cd0aaa09e8d3272146bc
oHTGo <nguyennhathuy.orit@gmail.com>
2023-11-08 01:04:13 +0700

    Fix CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path, { resolve } from 'path';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import manifest from './manifest';
@@ -57,13 +57,7 @@ export default defineConfig({
       output: {
         entryFileNames: 'src/pages/[name]/index.js',
         chunkFileNames: isDev ? 'assets/js/[name].js' : 'assets/js/[name].[hash].js',
-        assetFileNames: (assetInfo) => {
-          const { name, ext } = path.parse(assetInfo.name);
-          if (isDev && ext === '.css') {
-            return `assets/css/${name}${cacheInvalidationKey}.chunk.css`;
-          }
-          return `assets/[ext]/${name}.chunk.[ext]`;
-        },
+        assetFileNames: isDev ? 'assets/[ext]/[name].[ext]' : 'assets/[ext]/[name].[hash].[ext]',
       },
     },
   },
