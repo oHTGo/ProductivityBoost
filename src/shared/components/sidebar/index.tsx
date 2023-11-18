285324218ab7dade7592fcec67aa6b0c615d8381
oHTGo <nguyennhathuy.orit@gmail.com>
2023-11-18 23:40:37 +0700

    Add badge

--- src/shared/components/sidebar/index.tsx
+++ src/shared/components/sidebar/index.tsx
@@ -2,11 +2,13 @@ import Email from '@shared/components/email';
 import { variants, sizes, sidebarFeatures } from '@shared/components/sidebar/configurations';
 import useAppDispatch from '@shared/hooks/use-app-dispatch';
 import useAppSelector from '@shared/hooks/use-app-selector';
+import { getEmails } from '@shared/slices/email';
 import { collapse, expand, getIsExpanded, getIsOpen, getUI, setUI } from '@shared/slices/sidebar';
 import classNames from 'classnames';
 import { motion } from 'framer-motion';
 import { useRef, type FC } from 'react';
 import { useOnClickOutside } from 'usehooks-ts';
+import type { Feature } from '@shared/types/commons';
 
 type SidebarProps = {
   onClickOutside?: () => void;
@@ -16,14 +18,23 @@ const Sidebar: FC<SidebarProps> = ({ onClickOutside }) => {
   const isOpen = useAppSelector(getIsOpen);
   const isExpanded = useAppSelector(getIsExpanded);
   const ui = useAppSelector(getUI);
+  const emails = useAppSelector(getEmails);
   const dispatch = useAppDispatch();
 
-  const renderUI = () => {
-    switch (ui) {
+  const renderUI = (feature: Feature) => {
+    switch (feature) {
       case 'email':
         return <Email />;
       default:
         return <></>;
+    }
+  };
+  const renderBadge = (feature: Feature) => {
+    switch (feature) {
+      case 'email':
+        return `${emails.length}`;
+      default:
+        return '';
     }
   };
 
@@ -48,24 +59,36 @@ const Sidebar: FC<SidebarProps> = ({ onClickOutside }) => {
           isExpanded ? 'border-slate-300' : 'border-transparent',
         )}>
         {sidebarFeatures.map(({ Icon, feature }, key) => (
-          <div
-            className="flex items-center justify-center py-2"
-            key={key}
-            onClick={() => {
-              if (isExpanded) return dispatch(collapse());
+          <div className="relative w-11 h-11 py-2" key={key}>
+            <Icon
+              className={classNames(
+                'h-7 w-7 cursor-pointer rounded-md p-1 hover:bg-stone-300',
+                'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
+              )}
+              onClick={() => {
+                if (isExpanded) return dispatch(collapse());
 
-              dispatch(setUI(feature));
-              dispatch(expand());
-            }}>
-            <Icon className="h-7 w-7 cursor-pointer rounded-md p-1 hover:bg-stone-300" />
+                dispatch(setUI(feature));
+                dispatch(expand());
+              }}
+            />
+            {renderBadge(feature) !== '' && (
+              <span className="whitespace-nowrap w-4 h-4 rounded-sm bg-blue-300 text-sm text-white z-10 absolute top-1/2 left-1 inline-flex justify-center items-center">
+                {renderBadge(feature)}
+              </span>
+            )}
           </div>
         ))}
       </div>
       <motion.div
         initial={{ opacity: 0, width: 0 }}
-        animate={{ opacity: isExpanded ? 1 : 0, width: isExpanded ? sizes.md : '0' }}
-        className={classNames('h-full flex-1 border-solid border-0 border-slate-300', isExpanded ? ' border-l-1' : '')}>
-        {renderUI()}
+        animate={{
+          opacity: isExpanded ? 1 : 0,
+          width: isExpanded ? sizes.md : '0',
+          borderLeftWidth: isExpanded ? '1px' : '0px',
+        }}
+        className={classNames('h-full flex-1 border-solid border-0 border-slate-300')}>
+        {renderUI(ui as Feature)}
       </motion.div>
     </motion.div>
   );
