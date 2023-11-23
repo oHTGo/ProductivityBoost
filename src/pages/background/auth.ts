f10a26728269401563ef427093787fc68a8fb9e9
oHTGo <nguyennhathuy.orit@gmail.com>
2023-11-24 01:14:27 +0700

    Update auth

--- src/pages/background/auth.ts
+++ src/pages/background/auth.ts
@@ -1,12 +1,13 @@
 import { getConsentClientId } from '@shared/configurations/auth';
 import common from '@shared/constants/common';
-import { setLocalStorage } from '@shared/utils/storage';
+import { getLocalStorage, setLocalStorage } from '@shared/utils/storage';
 import ky from 'ky';
 import type { BackgroundFunction } from '@pages/background';
 
 const redirectUrl = chrome.identity.getRedirectURL('oauth2');
+const scopes = chrome.runtime.getManifest().oauth2?.scopes ?? [];
+
 const getToken = async (): Promise<string | undefined> => {
-  const scopes = chrome.runtime.getManifest().oauth2?.scopes ?? [];
   const response = await chrome.identity.launchWebAuthFlow({
     url:
       'https://accounts.google.com/o/oauth2/v2/auth?' +
@@ -27,6 +28,60 @@ const getToken = async (): Promise<string | undefined> => {
   return token;
 };
 
+const getAuthorizationCode = async (): Promise<string | undefined> => {
+  const clientId = await getLocalStorage(common.CUSTOM_CLIENT_ID);
+  if (!clientId) return;
+
+  const response = await chrome.identity.launchWebAuthFlow({
+    url:
+      'https://accounts.google.com/o/oauth2/v2/auth?' +
+      new URLSearchParams({
+        scope: scopes.join(' '),
+        response_type: 'code',
+        redirect_uri: redirectUrl,
+        client_id: clientId,
+        prompt: 'consent',
+        access_type: 'offline',
+      }).toString(),
+    interactive: true,
+  });
+  if (!response) return;
+
+  const params = new URLSearchParams(response.split('?')?.[1]);
+  const code = params.get('code') ?? '';
+
+  return code;
+};
+
+const getTokens = async (code: string): Promise<{ accessToken: string; refreshToken: string } | undefined> => {
+  const clientId = await getLocalStorage(common.CUSTOM_CLIENT_ID);
+  if (!clientId) return;
+
+  const clientSecret = await getLocalStorage(common.CUSTOM_CLIENT_SECRET);
+  if (!clientSecret) return;
+
+  const response = await ky
+    .post('https://oauth2.googleapis.com/token', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        code,
+        client_id: clientId,
+        client_secret: clientSecret,
+        redirect_uri: redirectUrl,
+        grant_type: 'authorization_code',
+      }).toString(),
+    })
+    .json<{ access_token: string; refresh_token: string }>();
+  if (!response) return;
+
+  const { access_token, refresh_token } = response;
+  if (!access_token || !refresh_token) return;
+
+  return { accessToken: access_token, refreshToken: refresh_token };
+};
+
 const getProfile = async (accessToken: string): Promise<{ id: string; name: string; email: string } | undefined> => {
   const response = await ky
     .get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json', {
@@ -41,7 +96,7 @@ const getProfile = async (accessToken: string): Promise<{ id: string; name: stri
   return { id, name, email };
 };
 
-export const auth: BackgroundFunction<void, void> = async () => {
+const defaultAuth = async (): Promise<void> => {
   const { token } = await chrome.identity.getAuthToken({
     interactive: true,
   });
@@ -62,4 +117,39 @@ export const auth: BackgroundFunction<void, void> = async () => {
   await setLocalStorage(common.USER_ID, id);
   await setLocalStorage(common.USER_NAME, name);
   await setLocalStorage(common.USER_EMAIL, email);
+};
+
+const customAuth = async (): Promise<void> => {
+  const code = await getAuthorizationCode();
+  if (!code) return;
+
+  const tokens = await getTokens(code);
+  if (!tokens) return;
+
+  const { accessToken, refreshToken } = tokens;
+  await setLocalStorage(common.ACCESS_TOKEN, accessToken);
+  await setLocalStorage(common.REFRESH_TOKEN, refreshToken);
+
+  const profile = await getProfile(accessToken);
+  if (!profile) return;
+
+  const { id, name, email } = profile;
+  await setLocalStorage(common.USER_ID, id);
+  await setLocalStorage(common.USER_NAME, name);
+  await setLocalStorage(common.USER_EMAIL, email);
+};
+
+export const auth: BackgroundFunction<void, void> = async () => {
+  await setLocalStorage(
+    common.CUSTOM_CLIENT_ID,
+    '883947601238-nfshkfrkmkasoc7p07hcv069jga030s7.apps.googleusercontent.com',
+  );
+  await setLocalStorage(common.CUSTOM_CLIENT_SECRET, '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');
+
+  const isCustomAuth =
+    Boolean(await getLocalStorage(common.CUSTOM_CLIENT_ID)) &&
+    Boolean(await getLocalStorage(common.CUSTOM_CLIENT_SECRET));
+
+  if (isCustomAuth) await customAuth();
+  else await defaultAuth();
 };
--- src/shared/clients/api.ts
+++ src/shared/clients/api.ts
@@ -1,25 +1,94 @@
 import common from '@shared/constants/common';
-import { getLocalStorage } from '@shared/utils/storage';
+import { getLocalStorage, setLocalStorage } from '@shared/utils/storage';
 import ky from 'ky';
+import type { NormalizedOptions } from 'ky';
+
+const getNewAccessToken = async (): Promise<string | undefined> => {
+  const refreshToken = await getLocalStorage(common.REFRESH_TOKEN);
+  if (!refreshToken) return;
+
+  const clientId = await getLocalStorage(common.CUSTOM_CLIENT_ID);
+  const clientSecret = await getLocalStorage(common.CUSTOM_CLIENT_SECRET);
+
+  const response = await ky
+    .post('https://oauth2.googleapis.com/token', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: clientId!,
+        client_secret: clientSecret!,
+        refresh_token: refreshToken,
+        grant_type: 'refresh_token',
+      }).toString(),
+    })
+    .json<{ access_token: string; refresh_token: string }>();
+
+  if (!response) return;
+
+  const { access_token } = response;
+  return access_token;
+};
+
+const defaultBeforeInterceptor = async (request: Request) => {
+  const userId = await getLocalStorage(common.USER_ID);
+  if (!userId) return request;
+
+  const { token } = await chrome.identity.getAuthToken({
+    interactive: false,
+    account: {
+      id: userId,
+    },
+  });
+  if (!token) return request;
+
+  request.headers.set('Authorization', `Bearer ${token}`);
+  return request;
+};
+const customBeforeInterceptor = async (request: Request) => {
+  const token = await getLocalStorage(common.ACCESS_TOKEN);
+  if (!token) return request;
+
+  request.headers.set('Authorization', `Bearer ${token}`);
+  return request;
+};
+const defaultAfterInterceptor = async (request: Request, options: NormalizedOptions, response: Response) => {
+  if (response.status === 401) {
+    const accessToken = await getNewAccessToken();
+    if (!accessToken) return new Response(null, { status: response.status });
+
+    await setLocalStorage(common.ACCESS_TOKEN, accessToken);
+    request.headers.set('Authorization', `Bearer ${accessToken}`);
+    return ky(request);
+  }
+
+  if (response.status >= 400) {
+    return new Response(null, { status: response.status });
+  }
+
+  return response;
+};
 
 const instance = ky.create({
   throwHttpErrors: false,
   hooks: {
     beforeRequest: [
       async (request) => {
-        const userId = await getLocalStorage(common.USER_ID);
-        if (!userId) return request;
+        const isCustomAuth =
+          Boolean(await getLocalStorage(common.CUSTOM_CLIENT_ID)) &&
+          Boolean(await getLocalStorage(common.CUSTOM_CLIENT_SECRET));
 
-        const { token } = await chrome.identity.getAuthToken({
-          interactive: false,
-          account: {
-            id: userId,
-          },
-        });
-        if (!token) return request;
-
-        request.headers.set('Authorization', `Bearer ${token}`);
-        return request;
+        if (isCustomAuth) return await customBeforeInterceptor(request);
+        return await defaultBeforeInterceptor(request);
+      },
+    ],
+    afterResponse: [
+      async (request, options, response) => {
+        const isCustomAuth =
+          Boolean(await getLocalStorage(common.CUSTOM_CLIENT_ID)) &&
+          Boolean(await getLocalStorage(common.CUSTOM_CLIENT_SECRET));
+        if (isCustomAuth) return await defaultAfterInterceptor(request, options, response);
+        return response;
       },
     ],
   },
