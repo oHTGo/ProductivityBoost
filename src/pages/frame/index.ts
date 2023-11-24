dc41253b20049ff78332bea810290399155e5194
oHTGo <nguyennhathuy.orit@gmail.com>
2023-11-25 01:26:07 +0700

    Update scrollbar

--- src/pages/frame/index.ts
+++ src/pages/frame/index.ts
@@ -1,16 +1,28 @@
 import { DEFAULT_SCROLLBAR_STYLES } from '@shared/configurations/twind';
+import refreshOnUpdate from 'virtual:reload-on-update-in-view';
+
+refreshOnUpdate('pages/newtab');
+
+const createScrollbarStyles = (document: Document | null) => {
+  const style = window.document.createElement('style');
+  style.innerHTML = DEFAULT_SCROLLBAR_STYLES.join('\n');
+  document?.head.appendChild(style);
+};
+document.addEventListener('DOMContentLoaded', () => {
+  createScrollbarStyles(document);
+});
 
 const params = new URLSearchParams(window.location.search);
 const src = decodeURIComponent(params.get('src') ?? '');
 
+const loader = document.querySelector('.loader') as HTMLDivElement;
 const iframe = document.createElement('iframe');
 iframe.src = src;
 iframe.style.visibility = 'hidden';
 iframe.addEventListener('load', () => {
-  const style = document.createElement('style');
-  style.innerHTML = DEFAULT_SCROLLBAR_STYLES.join('\n');
-  iframe.contentDocument?.head.appendChild(style);
+  createScrollbarStyles(iframe.contentDocument);
   iframe.style.visibility = 'visible';
+  loader.style.display = 'none';
 });
 
 document.body.appendChild(iframe);
