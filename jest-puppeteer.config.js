77af6ba83d1ec7555d08cb7851c561958c4b06b9
oHTGo <nguyennhathuy.orit@gmail.com>
2023-12-11 14:08:47 +0700

    Update test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,7 @@ import { join } from 'path';
 
 const EXTENSION_PATH = join(process.cwd(), 'dist');
 console.log('EXTENSION_PATH', EXTENSION_PATH);
+console.log('headless', process.env.CI ? 'new' : false);
 
 /** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
 export default {
