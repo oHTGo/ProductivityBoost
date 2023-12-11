9ea49f9e9afc06823e209b3a34aeacd180b70ca0
oHTGo <nguyennhathuy.orit@gmail.com>
2023-12-11 15:54:02 +0700

    Update test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -6,9 +6,7 @@ const EXTENSION_PATH = join(process.cwd(), 'dist');
 export default {
   launch: {
     dumpio: true,
-    headless: 'new',
-    timeout: 0,
-    executablePath: process.env.PUPPETEER_EXEC_PATH,
+    headless: process.env.CI ? 'new' : false,
     args: [
       '--disable-gpu',
       '--no-sandbox',
