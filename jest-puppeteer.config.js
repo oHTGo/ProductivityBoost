a22d50c719458aa7792fe470d8ae1e3bd9cda234
oHTGo <nguyennhathuy.orit@gmail.com>
2023-12-11 13:06:08 +0700

    Update test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,12 +1,12 @@
 import { join } from 'path';
 
 const EXTENSION_PATH = join(process.cwd(), 'dist');
+console.log('EXTENSION_PATH', EXTENSION_PATH);
 
 /** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
 export default {
   launch: {
-    // headless: process.env.CI ? 'new' : false,
-    headless: 'new',
+    headless: process.env.CI ? 'new' : false,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
