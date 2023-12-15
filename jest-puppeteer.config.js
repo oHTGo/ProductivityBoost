dd842edd2682a371eaf4093ed6c42ac06654f21b
oHTGo <nguyennhathuy.orit@gmail.com>
2023-12-16 01:43:00 +0700

    Update ci

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,19 +1,13 @@
-import { join } from 'path';
+// import { join } from 'path';
 
-const EXTENSION_PATH = join(process.cwd(), 'dist');
+// const EXTENSION_PATH = join(process.cwd(), 'dist');
 
 /** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
 export default {
   launch: {
     dumpio: true,
     headless: false,
-    args: [
-      '--disable-gpu',
-      '--disable-setuid-sandbox',
-      '--no-sandbox',
-      `--disable-extensions-except=${EXTENSION_PATH}`,
-      `--load-extension=${EXTENSION_PATH}`,
-      '--load-guest-mode-test-extension',
-    ],
+    executablePath: '/usr/bin/chromium-browser',
+    args: ['--disable-gpu', '--disable-setuid-sandbox', '--no-sandbox'],
   },
 };
