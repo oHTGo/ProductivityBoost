95bed85b5c217da46eae8cd1ab1edc2b6d1daf24
oHTGo <nguyennhathuy.orit@gmail.com>
2023-12-17 20:58:29 +0700

    Update ci

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -7,15 +7,12 @@ export default {
   launch: {
     dumpio: true,
     headless: false,
-    timeout: 0,
-    executablePath: process.env.PUPPETEER_EXEC_PATH,
     args: [
       '--disable-gpu',
       '--no-sandbox',
       '--disable-setuid-sandbox',
       `--load-extension=${EXTENSION_PATH}`,
       `--disable-extensions-except=${EXTENSION_PATH}`,
-      '--window-size=1280,1024',
     ],
   },
 };
