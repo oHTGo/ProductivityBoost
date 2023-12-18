d9f978aa8765affb53b084b857f7df20ec8d84a6
oHTGo <nguyennhathuy.orit@gmail.com>
2023-12-18 09:55:31 +0700

    Update ci

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -10,11 +10,9 @@ export default {
     ignoreHTTPSErrors: true,
     args: [
       '--disable-gpu',
-      '--disable-dev-shm-usage',
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-web-security',
-      '--enable-automation',
       `--load-extension=${EXTENSION_PATH}`,
       `--disable-extensions-except=${EXTENSION_PATH}`,
     ],
