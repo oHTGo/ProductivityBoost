b6a10531bd4a820442a8ccfead6b7b1b875f9541
oHTGo <nguyennhathuy.orit@gmail.com>
2024-02-17 21:16:09 +0700

    Enhance ui

--- src/pages/newtab/newtab.tsx
+++ src/pages/newtab/newtab.tsx
@@ -20,12 +20,12 @@ import type { DotLottieCommonPlayer } from '@dotlottie/react-player';
 import type { ICredential } from '@shared/types/auth';
 import type { IEmail } from '@shared/types/email';
 
-const LOGIN_FRAMES = {
-  LOG_IN: {
+const ANIMATION_FRAMES = {
+  LOGIN: {
     start: 0,
     end: 112,
   },
-  LOG_OUT: {
+  LOGOUT: {
     start: 113,
     end: 181,
   },
@@ -33,7 +33,7 @@ const LOGIN_FRAMES = {
 
 const NewTab = () => {
   const loginRef = useRef<DotLottieCommonPlayer>(null);
-  const [loginState, setLoginState] = useState<keyof typeof LOGIN_FRAMES>();
+  const [loginState, setLoginState] = useState<keyof typeof ANIMATION_FRAMES>();
 
   const dispatch = useAppDispatch();
   const emailCallback = (emails: IEmail[]) => dispatch(setEmails(emails));
@@ -44,7 +44,7 @@ const NewTab = () => {
 
   useEffect(() => {
     if (!loginState) return;
-    loginRef.current?.playSegments([LOGIN_FRAMES[loginState].start, LOGIN_FRAMES[loginState].end], true);
+    loginRef.current?.playSegments([ANIMATION_FRAMES[loginState].start, ANIMATION_FRAMES[loginState].end], true);
   }, [loginState]);
 
   useEffect(() => {
@@ -62,9 +62,14 @@ const NewTab = () => {
           className="w-32 h-32"
           onEvent={(e) => {
             if (e !== PlayerEvents.Ready) return;
-            checkLoggedIn((isLoggedIn) => setLoginState(isLoggedIn ? 'LOG_IN' : 'LOG_OUT'));
+            checkLoggedIn((isLoggedIn) => setLoginState(isLoggedIn ? 'LOGIN' : 'LOGOUT'));
           }}
-          src={chrome.runtime.getURL('assets/lotties/login.lottie')}></DotLottiePlayer>
+          src={chrome.runtime.getURL('assets/lotties/login.lottie')}
+        />
+        {loginState === 'LOGIN' ? <span className="prose prose-neutral">Logged in</span> : null}
+        {loginState === 'LOGOUT' ? (
+          <span className="prose prose-neutral">Please login to use this extension</span>
+        ) : null}
         <TextField
           id="client-id"
           label="Client ID"
@@ -85,25 +90,25 @@ const NewTab = () => {
               label: 'Login',
               onClick: () => {
                 login((isSuccess) => {
-                  if (!isSuccess) return setLoginState('LOG_OUT');
-                  setLoginState('LOG_IN');
+                  if (!isSuccess) return setLoginState('LOGOUT');
+                  setLoginState('LOGIN');
                   fetchEmails(emailCallback);
                 });
               },
-              active: loginState === 'LOG_IN',
-              disabled: loginState === 'LOG_IN',
+              active: loginState === 'LOGIN',
+              disabled: loginState === 'LOGIN',
             },
             {
               label: 'Logout',
               onClick: () => {
                 logout((isSuccess) => {
-                  if (!isSuccess) return setLoginState('LOG_IN');
-                  setLoginState('LOG_OUT');
+                  if (!isSuccess) return setLoginState('LOGIN');
+                  setLoginState('LOGOUT');
                   emailCallback([]);
                 });
               },
-              active: loginState === 'LOG_OUT',
-              disabled: loginState === 'LOG_OUT',
+              active: loginState === 'LOGOUT',
+              disabled: loginState === 'LOGOUT',
             },
           ]}
         />
